4a6ebd4cd71289bb0591bdb3495212f3049464dc
DESKTOP-BO8A4SQ\renan <renan1504@gmail.com>
2018-06-26 23:41:34 -0300

    Vulcão

---
+++ Tanks/Assets/_Completed-Assets/Scripts/Volcano/RockBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBehaviour : MonoBehaviour {
+
+    public Vector3 Impulse { get; set; }
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody.GetComponent<Rigidbody>().AddForce(Impulse, ForceMode.Impulse);
+    }
+}
---
+++ Tanks/Assets/_Completed-Assets/Scripts/Volcano/VolcanoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolcanoBehaviour : MonoBehaviour {
+
+    public Transform SpawnPoint;
+    public float ThrowRate = 10f;
+    public GameObject Rock;
+    private Vector3 _impulse;
+    private float _timer;
+	private void Start () {
+		
+	}
+	
+	private void Update () {
+        _timer -= Time.deltaTime;
+        if (_timer< 0)
+        {
+            ThrowRock();
+            _timer = ThrowRate;
+        }
+	}
+
+    private void GenerateImpulse()
+    {
+        var x = 10f;
+        var y = 10f;
+        var z = 10f;
+
+
+    }
+
+    private void ThrowRock()
+    {
+        GenerateImpulse();
+        var obj = Instantiate(Rock,SpawnPoint.position,Quaternion.identity);
+        obj.GetComponent<RockBehaviour>().Impulse = _impulse;
+    }
+}
